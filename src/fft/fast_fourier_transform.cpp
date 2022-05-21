659adaa16f53360e05fbe1bcf5de0e9f18648a3e
Paul Galand <paul.galand@epita.fr>
2022-05-21 16:10:24 +0200

    A: fast_fourier_transform.cpp
    fft computation implementation

---
+++ src/fft/fast_fourier_transform.cpp
@@ -0,0 +1,72 @@
+# include "fast_fourier_transform.hpp"
+
+void FFT::sort() {
+    for (int i = 0, nc = n; i < p - 1; i++, nc /= 2) {
+        std::vector<double> r_sort;
+        std::vector<double> i_sort;
+
+        r_sort.reserve(n);
+        i_sort.reserve(n);
+
+        std::vector<double> r1(nc / 2);
+        std::vector<double> r2(nc / 2);
+        std::vector<double> i1(nc / 2);
+        std::vector<double> i2(nc / 2);
+
+        std::vector<double>::iterator r_iter(r_sort.begin());
+        std::vector<double>::iterator i_iter(i_sort.begin());
+        for (int j = 0; j < n / nc; j++) {
+            for (int k = 0; k < nc / 2; k++) {
+                const double ii = 2 * k + j * nc;
+                r1[k] = r_values->at(ii);
+                r2[k] = r_values->at(ii + 1);
+                i1[k] = i_values->at(ii);
+                i2[k] = i_values->at(ii + 1);
+            }
+            r_sort.insert(r_iter, r1.begin(), r1.end());
+            r_sort.insert(r_sort.end(), r2.begin(), r2.end());
+            r_sort.insert(i_iter, i1.begin(), i1.end());
+            r_sort.insert(i_sort.end(), i2.begin(), i2.end());
+
+            r_iter = r_sort.end();
+            i_iter = i_sort.end();
+        }
+        swap(r_sort, *r_values);
+        swap(i_sort, *i_values);
+        nc /= 2;
+    }
+}
+
+void FFT::compute(bool reverse) {
+    sort();
+
+    std::vector<double> rvc(n);
+    std::vector<double> ivc(n);
+
+    const int inv = reverse ? 1 : -1;
+
+    for (int i = 0, nc = n; i < p; i++, nc /= 2) {
+        for (int j = 0; j < nc / 2; j++) {
+            int powi = pow(2, i);
+            for (int k = 0; k < powi; k++) {
+                const int ii = k + 2 * j * powi;
+                const int jj = ii + powi;
+                const double pos = inv * ii * M_PI / powi;
+
+                const double pcos = cos(pos);
+                const double psin = sin(pos);
+                const double r1 = r_values->at(ii);
+                const double i1 = i_values->at(ii);
+                const double r2 = r_values->at(jj);
+                const double i2 = i_values->at(jj);
+
+                rvc[ii] = r1 + pcos * r2 - psin * i2;
+                ivc[ii] = i1 + pcos * i2 + psin * r2;
+                rvc[jj] = r1 - pcos * r2 + psin * i2;
+                ivc[jj] = i1 - pcos * i2 - psin * r2;
+            }
+        }
+        swap(rvc, *r_values);
+        swap(ivc, *i_values);
+    }
+}
---
+++ src/fft/fast_fourier_transform.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cmath>
+#include <vector>
+
+class FFT {
+    private:
+        const int n;
+        const int p;
+
+        std::vector<double>* r_values;
+        std::vector<double>* i_values;
+
+        void sort();
+    
+    public:
+        FFT(const int n, std::vector<double>* const r_values, std::vector<double>* const i_values)
+            : n(n)
+            , p(log2(n))
+            , r_values(r_values)
+            , i_values(i_values)
+        {}
+        
+        void compute(bool reverse);
+};
